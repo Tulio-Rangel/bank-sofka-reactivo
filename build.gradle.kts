118881b0e00f7ee824d3630d4e266f9abc9bc53d
Tulio-Rangel <tjrn20@hotmail.com>
2025-01-16 09:28:23 -0500

    feat: add swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.projectreactor:reactor-core")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.21")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/java/com/tulio/banksofkareactivo/BankSofkaReactivoApplication.java
+++ src/main/java/com/tulio/banksofkareactivo/BankSofkaReactivoApplication.java
@@ -1,11 +1,11 @@
 package com.tulio.banksofkareactivo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
-@EnableWebFlux
+@OpenAPIDefinition
 public class BankSofkaReactivoApplication {
 
     public static void main(String[] args) {
